3e7782a4b7b395f202b776c29d81d9eb6322d4ac
Shelby Soutter <ssoutter@AP-SSOUTTER.local>
2022-02-22 14:35:47 -0700

    Updated master_authorized_networks

--- main.tf
+++ main.tf
@@ -110,7 +110,12 @@ module "gke" {
   enable_private_endpoint    = true
   enable_private_nodes       = true
   master_ipv4_cidr_block     = "10.0.0.0/28"
-  master_authorized_networks = ["172.27.32.14/32"]
+  master_authorized_networks = [
+    {
+      cidr_block = "172.27.32.14/32" 
+      display_name = "dev-gen3-workspace-1-vpc"
+    },
+  ]
   create_service_account     = true
   regional                   = true
 
