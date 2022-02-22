1e4739a0d5e9772067a5c2c6a6174cff1680b0dd
Shelby Soutter <ssoutter@AP-SSOUTTER.local>
2022-02-22 13:28:44 -0700

    Updated master authorized networks with proxy IP

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ module "gke" {
   enable_private_endpoint    = true
   enable_private_nodes       = true
   master_ipv4_cidr_block     = "10.0.0.0/28"
-#   master_authorized_networks = ["66.60.120.90"]
+  master_authorized_networks = ["172.27.32.14"]
   create_service_account     = true
   regional                   = true
 
